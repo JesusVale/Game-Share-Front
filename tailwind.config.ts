2e0884f971f63139524dfc7f6364a1bdf53d2958
JesusVale <108861698+JesusVale@users.noreply.github.com>
2024-01-05 21:51:03 -0700

    config: add tailwind initial configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,15 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'main-gradient': 'linear-gradient(184deg, rgba(22,22,22,1) 69%, rgba(0,0,0,1) 100%)'
       },
+      colors: {
+        'primary-color': '#161616',
+        'line-gray': '#686868',
+        'secondary-color': '##343434',
+        'main-gray': '#A7A7A7',
+        'shade-gray': '#616161'
+      }
     },
   },
   plugins: [],
